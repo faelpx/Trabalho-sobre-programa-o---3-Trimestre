7cd8b9923be870762a068490c503ec06bb5daa0d
rafaelpradx <140827409+faelpx@users.noreply.github.com>
2024-09-21 02:36:17 -0300

    Add files via upload

---
+++ Trabalho sobre o que é programação/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que são variáveis?',
+    'Variáveis são espaços na memória que armazenam dados que podem ser alterados durante a execução de um programa.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função??',
+    'Uma função é um bloco de código que executa uma tarefa específica e pode ser reutilizado em diferentes partes do programa.'
+)
